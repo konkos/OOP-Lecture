9904c2f3ab3c8a1ff007a4382aed6cd161efcaff
konkos <kon_kostas@windowslive.com>
2022-08-21 18:15:37 +0300

    Intro to GUI

---
+++ src/GUI/intro/GUI.java
@@ -0,0 +1,16 @@
+package GUI.intro;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    private JPanel panel;
+
+    public GUI(){
+        panel = new JPanel();
+
+        this.setContentPane(panel);
+        this.setVisible(true);
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
